4cbdc2a717f182be8081fc2e02b4ef4516a89917
aniketz18 <aniketzade700@gmail.com>
2026-09-23 01:01:24 +0530

    feat: integrate Firebase authentication  clientside

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,18 @@
 import { useState } from 'react'
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Home from '../pages/Home'
+import Auth from '../pages/Auth'
 
 function App() {
 
   return (
-    <>
-     hello
-    </>
+    
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/auth' element={<Auth/>}/>
+    </Routes>
+  
   )
 }
 

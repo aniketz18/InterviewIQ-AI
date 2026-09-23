6c74b71b6adb7056b81deb4f507d86fad37a8857
aniketz18 <aniketzade700@gmail.com>
2026-09-23 22:38:28 +0530

    fix: correct jwt function call in token generation

--- server/configs/generateToken.js
+++ server/configs/generateToken.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 
 const generateToken = async (userId) => {
   try {
-    const token = await jwt({ userId }, process.env.JWT_SECRET, {
+    const token = await jwt.sign({ userId }, process.env.JWT_SECRET, {
       expiresIn: "7d",
     });
     return token;
